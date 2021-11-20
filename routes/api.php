4529206446f671b50ec67ce6b220929c0cf9484b
Jean Franco <jean.f.ponte.solis@gmail.com>
2021-11-19 20:58:34 -0500

    second commit

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::group(['middleware' => ['cors']], function () {
-    //Rutas a las que se permitirá acceso
     
+    
+
 });
